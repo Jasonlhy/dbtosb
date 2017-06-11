5621c8f010100d83fe8789392208768a6d74fd07
Jason Liu <j-lhy@hotmail.com>
2017-06-11 17:29:14 +0800

    add option to cli

--- bin/dbtosb.js
+++ bin/dbtosb.js
@@ -1,19 +1,106 @@
 #!/usr/bin/env node
 var process = require('process');
+var fs = require("fs");
 var dbtosb = require("../dbtosb.js");
+var argv = require('yargs').options({
+    'f': {
+        alias: 'fromEncoding',
+        type: 'string'
+    },
+    't': {
+        alias: 'toEncoding',
+        type: 'string'
+    },
+    'o': {
+        alias: 'outputFile',
+        type: 'string'
+    },
+}).argv;
 
-var incode = process.argv[2] || 'utf8';
-var outcode = process.argv[3];
+(function () {
+    /* Command Line Arguments */
+    var IN_ENCODING = argv['f'] || 'utf8';
+    var OUT_ENCODING = argv['t'];
+    var OUT_FILE_PATH = argv['o'];
+    var IN_FILE_PATH = argv._[0];
 
-process.stdin.setEncoding(incode);
-if (outcode){
-    process.stdout.setEncoding(outcode);
-}
-
-process.stdin.on('readable', () => {
-    const chunk = process.stdin.read();
-    if (chunk !== null) {
-        var output = dbtosb.convert(chunk);
-        process.stdout.write(output);
+    /**
+     * Read text from Standard Input
+     * 
+     * @param {function} outputCallback accept data as string
+     */
+    function readStdin(outputCallback) {
+        process.stdin.setEncoding(IN_ENCODING);
+        process.stdin.on('readable', () => {
+            const chunk = process.stdin.read();
+            outputCallback(chunk);
+        });
     }
-});
+
+    /**
+     * Read text from File
+     * 
+     * @param {string} filePath 
+     * @param {function} outputCallback accept data as string
+     */
+    function readFile(filePath, outputCallback) {
+        fs.readFile(filePath, { encoding: IN_ENCODING }, function (err, data) {
+            if (err) throw err;
+            outputCallback(data);
+        });
+    }
+
+    /**
+     * Convert the text and output to file
+     * 
+     * @param {string} filePath 
+     * @param {string} data Double Byte string data
+     */
+    function convertToFile(filePath, data) {
+        var options = {};
+        if (OUT_ENCODING){
+            options['encoding'] = OUT_ENCODING;
+        }
+
+        var output = dbtosb.convert(data);
+        fs.writeFile(filePath, output, options, function (err) {
+            if (err) throw err;
+        });
+    }
+
+    /**
+     * Convert the text and output to standard output
+     * 
+     * @param {string} data Double Byte string data
+     */
+    function convertToStdOut(data) {
+        if (OUT_ENCODING) {
+            process.stdout.setEncoding(OUT_ENCODING);
+        }
+
+        if (data !== null) {
+            var output = dbtosb.convert(data);
+            process.stdout.write(output);
+        }
+    }
+
+    /**
+     * Output callback after input
+     * 
+     * @param {string} data 
+     */
+    function output(data) {
+        if (OUT_FILE_PATH) {
+            convertToFile(OUT_FILE_PATH, data);
+        } else {
+            convertToStdOut(data);
+        }
+    }
+
+    /** Input */
+    if (IN_FILE_PATH) {
+        readFile(IN_FILE_PATH, output)
+    } else {
+        readStdin(output);
+    }
+})();
